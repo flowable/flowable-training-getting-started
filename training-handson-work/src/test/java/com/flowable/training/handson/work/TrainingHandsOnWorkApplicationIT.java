5fdc32bfcfcd0c3be6d86e3a01ac20911f312966
Matthias Stöckli <matthias.stoeckli@flowable.com>
2022-10-18 09:37:45 +0200

    Remove test profile

--- training-handson-work/src/test/java/com/flowable/training/handson/work/TrainingHandsOnWorkApplicationIT.java
+++ training-handson-work/src/test/java/com/flowable/training/handson/work/TrainingHandsOnWorkApplicationIT.java
@@ -1,14 +1,11 @@
 package com.flowable.training.handson.work;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
-class TrainingHandsOnWorkApplicationTest {
+class TrainingHandsOnWorkApplicationIT {
 
 }
