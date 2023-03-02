705db17da63a9a7d3a8532793c9f4727cccdf3ba
matthias.stoeckli <matthias.stoeckli@flowable.com>
2023-03-02 14:00:38 +0100

    Add required annotation to test

--- training-handson-work/src/test/java/com/flowable/training/handson/work/task/CreditCardValidationTaskTest.java
+++ training-handson-work/src/test/java/com/flowable/training/handson/work/task/CreditCardValidationTaskTest.java
@@ -19,10 +19,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 @ExtendWith(FlowableSpringExtension.class)
 class CreditCardValidationTaskTest {
 
