438fe1f86fbc2efd11f808fe4effd92f248ff90a
matthias.stoeckli <matthias.stoeckli@flowable.com>
2023-03-02 13:37:14 +0100

    Update to 3.13.2

--- training-handson-work/src/main/java/com/flowable/training/handson/work/configuration/SecurityActuatorConfiguration.java
+++ training-handson-work/src/main/java/com/flowable/training/handson/work/configuration/SecurityActuatorConfiguration.java
@@ -3,7 +3,9 @@ package com.flowable.training.handson.work.configuration;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.actuate.logging.LogFileWebEndpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,16 +14,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 import com.flowable.actuate.autoconfigure.security.servlet.ActuatorRequestMatcher;
 import com.flowable.platform.common.security.SecurityConstants;
+import org.springframework.security.web.SecurityFilterChain;
 
-@ConditionalOnClass(EndpointRequest.class)
-@Configuration(proxyBeanMethods = false)
-@Order(6) // Actuator configuration should kick in before the Form Login there should always be http basic for the endpoints
-@SuppressWarnings("deprecation")
-public class SecurityActuatorConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityActuatorConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
+    @Bean
+    @Order(6)
+    // Actuator configuration should kick in before the Form Login there should always be HTTP basic for the endpoints
+    public SecurityFilterChain basicActuatorSecurity(HttpSecurity http) throws Exception {
         http
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -32,9 +33,11 @@ public class SecurityActuatorConfiguration extends WebSecurityConfigurerAdapter 
         http
                 .requestMatcher(new ActuatorRequestMatcher())
                 .authorizeRequests()
-                .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
+                .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class, LogFileWebEndpoint.class)).permitAll()
                 .requestMatchers(EndpointRequest.toAnyEndpoint()).hasAuthority(SecurityConstants.ACCESS_ACTUATORS)
                 .anyRequest().denyAll()
                 .and().httpBasic();
+
+        return http.build();
     }
 }
