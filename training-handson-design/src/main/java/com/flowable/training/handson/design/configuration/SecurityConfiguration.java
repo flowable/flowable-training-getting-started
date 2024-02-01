7b6c1998d68bc905b0d7eebb7e85d1e35fc01fac
matthias.stoeckli <matthias.stoeckli@flowable.com>
2024-02-01 17:33:35 +0100

    Upgrade to 3.15 and update documentation

--- training-handson-design/src/main/java/com/flowable/training/handson/design/configuration/SecurityConfiguration.java
+++ training-handson-design/src/main/java/com/flowable/training/handson/design/configuration/SecurityConfiguration.java
@@ -1,15 +1,12 @@
-package com.flowable.training.handson.work.configuration;
+package com.flowable.training.handson.design.configuration;
 
-import com.flowable.autoconfigure.frontend.FrontendProperties;
-import com.flowable.autoconfigure.security.FlowableHttpSecurityCustomizer;
-import com.flowable.core.spring.security.web.authentication.AjaxAuthenticationFailureHandler;
-import com.flowable.core.spring.security.web.authentication.AjaxAuthenticationSuccessHandler;
-import com.flowable.platform.common.security.SecurityConstants;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import java.util.stream.Collectors;
 
 import jakarta.servlet.DispatcherType;
 
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -17,47 +14,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.security.web.util.matcher.DispatcherTypeRequestMatcher;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import com.flowable.autoconfigure.design.security.DesignHttpSecurityCustomizer;
+import com.flowable.design.security.spring.web.authentication.AjaxAuthenticationFailureHandler;
+import com.flowable.design.security.spring.web.authentication.AjaxAuthenticationSuccessHandler;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Autowired
-    protected ObjectProvider<FrontendProperties> frontendPropertiesProvider;
-
     @Bean
     @Order(10)
-    public SecurityFilterChain basicDefaultSecurity(HttpSecurity http, ObjectProvider<FlowableHttpSecurityCustomizer> httpSecurityCustomizers)
-            throws Exception {
-        for (FlowableHttpSecurityCustomizer customizer : httpSecurityCustomizers.orderedStream().toList()) {
+    public SecurityFilterChain basicDefaultSecurity(HttpSecurity http, ObjectProvider<DesignHttpSecurityCustomizer> httpSecurityCustomizers) throws Exception {
+        for (DesignHttpSecurityCustomizer customizer : httpSecurityCustomizers.orderedStream()
+                .collect(Collectors.toList())) {
             customizer.customize(http);
         }
 
         http
-                .logout(logout -> logout.logoutUrl("/auth/logout"));
-
-        FrontendProperties frontendProperties = frontendPropertiesProvider.getIfAvailable();
-        if (frontendProperties != null && frontendProperties.getFeatures().containsKey("formBasedLogout")
-                && frontendProperties.getFeatures().get("formBasedLogout")) {
-            http
-                    .logout(logout -> logout.logoutSuccessUrl("/"));
-        } else {
-            http
-                    .logout(logout -> logout.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()));
-        }
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/")
+                );
 
         http
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         // using this no op authentication entry point until https://github.com/spring-projects/spring-boot/issues/36948 gets resolved
-                        .defaultAuthenticationEntryPointFor((request, response, authException) -> {
-                        }, new DispatcherTypeRequestMatcher(DispatcherType.ERROR))
+                        .defaultAuthenticationEntryPointFor((request, response, authException) -> {}, new DispatcherTypeRequestMatcher(DispatcherType.ERROR))
                         .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), AnyRequestMatcher.INSTANCE))
                 .formLogin(formLogin -> formLogin
                         .loginProcessingUrl("/auth/login")
@@ -65,8 +55,6 @@ public class SecurityConfiguration {
                         .failureHandler(new AjaxAuthenticationFailureHandler())
                 )
                 .authorizeHttpRequests(configurer -> configurer
-                        .requestMatchers(antMatcher("/analytics-api/**")).hasAuthority(SecurityConstants.ACCESS_REPORTS_METRICS)
-                        .requestMatchers(antMatcher("/work-object-api/**")).hasAuthority(SecurityConstants.ACCESS_WORKOBJECT_API)
                         // allow context root for all (it triggers the loading of the initial page)
                         .requestMatchers(antMatcher("/")).permitAll()
                         .requestMatchers(
