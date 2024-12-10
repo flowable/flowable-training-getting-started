b824df64afcddcece9df177bee100437539daf76
matthias.stoeckli <matthias.stoeckli@flowable.com>
2024-12-10 11:29:36 +0100

    Add GSD3

--- flowable-work/src/main/java/com/flowable/gsd/work/task/CreditCardValidationTask.java
+++ flowable-work/src/main/java/com/flowable/gsd/work/task/CreditCardValidationTask.java
@@ -1,9 +1,10 @@
 package com.flowable.gsd.work.task;
 
 import org.flowable.common.engine.api.variable.VariableContainer;
-import org.flowable.engine.delegate.BpmnError;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.flowable.gsd.work.service.creditcard.CreditCardService;
 import com.flowable.gsd.work.service.creditcard.ValidationResult;
 import com.flowable.platform.tasks.AbstractPlatformTask;
@@ -17,9 +18,11 @@ import com.flowable.platform.tasks.ExtensionElementsContainer;
 public class CreditCardValidationTask extends AbstractPlatformTask {
 
     private final CreditCardService creditCardService;
+    private final ObjectMapper objectMapper;
 
-    public CreditCardValidationTask(CreditCardService creditCardService) {
+    public CreditCardValidationTask(CreditCardService creditCardService, ObjectMapper objectMapper) {
         this.creditCardService = creditCardService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -28,11 +31,11 @@ public class CreditCardValidationTask extends AbstractPlatformTask {
         String creditCardNumber = (String) variableContainer.getVariable("creditCardNumber");
         ValidationResult validationResult = creditCardService.validateCreditCard(creditCardNumber);
 
-        if(!validationResult.isValid()) {
-            throw new BpmnError("creditCardInvalid", validationResult.failReason());
-        }
-
         // We are setting a variable name directly, this is easy but not really recommended.
         variableContainer.setVariable("creditCardValid", true);
+
+        // Alternatively, we can also set the whole result as a JSON
+        ObjectNode validationResultJson = objectMapper.valueToTree(validationResult);
+        variableContainer.setVariable("creditCardValidationResult", validationResultJson);
     }
 }
