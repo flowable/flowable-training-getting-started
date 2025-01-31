ed21eb14c595ae6c2a8f62568aca6492a726f645
matthias.stoeckli <matthias.stoeckli@flowable.com>
2025-01-31 15:56:53 +0100

    Delete Frontend

--- frontend/lib/components/wizard/react/KtagWizard.tsx
+++
@@ -1,2 +0,0 @@
-const KtagWizard = () => <div style={{width: '300px', backgroundColor: 'red'}}><h1>Hello World</h1></div>
-export default KtagWizard;
--- frontend/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import KtagWizard from './components/wizard/react/KtagWizard';
-
-export default {
-    ktagWizard: KtagWizard
-};
