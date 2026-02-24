7b8314aab9d6ee9ac5978de1885603f788e06ba6
Matthias Stöckli <matthias.stoeckli@flowable.com>
2026-02-24 18:28:40 +0100

    Add GSD2a

--- flowable-work/src/main/java/com/flowable/gsd/work/card/FlowableCreditCardService.java
+++ flowable-work/src/main/java/com/flowable/gsd/work/card/FlowableCreditCardService.java
@@ -1,17 +1,36 @@
 package com.flowable.gsd.work.card;
 
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.node.ObjectNode;
 
 @Service("flowableCreditCardService")
 public class FlowableCreditCardService {
 
     private final CardValidatorService cardValidatorService;
+    private final ObjectMapper objectMapper;
 
-    public FlowableCreditCardService(CardValidatorService cardValidatorService) {
+    public FlowableCreditCardService(CardValidatorService cardValidatorService, ObjectMapper objectMapper) {
         this.cardValidatorService = cardValidatorService;
+        this.objectMapper = objectMapper;
     }
 
     public boolean isCardValid(String cardNumber) {
         return cardValidatorService.validateCreditCard(new CreditCard(cardNumber, null, null)).isValid();
+    }
+
+    public ObjectNode validateCreditCard(String cardNumber) {
+        CreditCard creditCard = new CreditCard(cardNumber, null, null);
+        ValidationResult validationResult = cardValidatorService.validateCreditCard(creditCard);
+        ObjectNode jsonResult = objectMapper.createObjectNode();
+        jsonResult.put("isValid", validationResult.isValid());
+
+        if (validationResult.isValid()) {
+            jsonResult.put("message", "Credit card valid");
+        } else {
+            jsonResult.put("message", "Credit card invalid");
+        }
+
+        return jsonResult;
     }
 }
